255c9a4fb8adfd47801e29c152684d0b8cc26030
unknown <erfan8202@gmail.com>
2022-09-02 02:05:20 +0430

    class name refactor

--- components/Main/InputHolder.jsx
+++ components/Main/InputHolder.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
 
-const InputHolder = ({inputName,img,alt,op1,op2}) => {
+const InputHolder = ({ inputName, img, alt, op1, op2 }) => {
   return (
     <>
-       <p className="mb-1">{inputName}</p>
-            <div className={`flex items-center bg-sp-gray  h-[35px]`}>
-              <div className="ml-3 justify-self-center">
-              <Image
-                src={img}
-                width={24}
-                height={24}
-                alt={alt}
-                />
-                </div>
-              <select defaultValue={'DEFAULT'} className="appearance-none bg-inherit w-full">
-                <option value="DEFAULT" hidden disabled  ></option>
-                <option>{op1}</option>
-                <option>{op2}</option>
-              </select>
-            </div>
+      <p className="mb-1">{inputName}</p>
+      <div className='flex items-center bg-sp-gray  h-[35px]'>
+        <div className="ml-3 justify-self-center">
+          <Image
+            src={img}
+            width={24}
+            height={24}
+            alt={alt}
+          />
+        </div>
+        <select defaultValue={'DEFAULT'} className="appearance-none bg-inherit w-full">
+          <option value="DEFAULT" hidden disabled  ></option>
+          <option>{op1}</option>
+          <option>{op2}</option>
+        </select>
+      </div>
     </>
   )
 }
