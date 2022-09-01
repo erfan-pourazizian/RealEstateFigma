0a1b8317980400c4f05eb920fad3fff367955398
unknown <erfan8202@gmail.com>
2022-09-02 02:12:46 +0430

    property completed

--- components/FeaturedProperties/Property.jsx
+++ components/FeaturedProperties/Property.jsx
@@ -28,10 +28,61 @@ const Property = ({
                         alt="house"
                     />
                 </div>
-                <div className='flex pt-2 items-center justify-between'>
-                    <div className='flex items-center'>
+                <div className='mt-4'>
+                    <p className='text-lg text-sp-gray2 text-bold'>
+                        {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+                    </p>
+                    <div className='flex items-center mt-1'>
 
+                        <div className="mr-3 flex self-end">
+                            <Image
+                                src="/location.png"
+                                width={20}
+                                height={20}
+                                alt='location'
+                            />
+                        </div>
+                        <p className="text-contactSize text-sp-gray2 ">example , example</p>
                     </div>
+                    <div className="flex mt-2 ">
+
+                        <div className="flex">
+                            <div className="mr-3 flex self-end ">
+                                <Image
+                                    src="/sqft.png"
+                                    width={20}
+                                    height={20}
+                                    alt='sqft'
+                                />
+                            </div>
+                            <p className="text-contactSize text-sp-gray2 ">{area} sqft</p>
+                        </div>
+
+                        <div className="flex">
+                            <div className="mr-3 ml-4 flex self-end ">
+                                <Image
+                                    src="/beds.png"
+                                    width={20}
+                                    height={20}
+                                    alt='beds'
+                                />
+                            </div>
+                            <p className="text-contactSize text-sp-gray2 ">{rooms} </p>
+                        </div>
+
+                        <div className="flex">
+                            <div className="mr-3 ml-7 flex self-end ">
+                                <Image
+                                    src="/bath.png"
+                                    width={20}
+                                    height={20}
+                                    alt='bath'
+                                />
+                            </div>
+                            <p className="text-contactSize text-sp-gray2 ">{baths} </p>
+                        </div>
+                    </div>
+                    <h3 className="text-3xl text-sp-orange mt-3">$ 2000</h3>
                 </div>
             </div>
         </Link>
