4d108ef010f32b39823721779fc5e37873b32974
unknown <erfan8202@gmail.com>
2022-09-02 17:46:59 +0430

    font removed

--- components/FeaturedProperties/FeaturedProperties.jsx
+++ components/FeaturedProperties/FeaturedProperties.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import Property from './Property'
 import {baseUrl, fetchApi} from "../../utils/fetchApi";
+import DefaultImage from "../../public/Images/default-house.png"
 
 const FeaturedProperties = ({ propertiesForRent }) => {
+  const property = {
+      coverPhoto : {
+        url : DefaultImage
+      },
+      price : '2000',
+      rooms: '4',
+      title: 'Songgoriti villa aduhai',
+      baths: '4',
+      area: '4',
+      externalID: '1926',
+  }
   console.log(`${propertiesForRent}<=======result`); //  undefined mide
   return (
     <div className='mt-36'>
       <h1 className='text-spSize1 text-sp-gray2 text-center'>Featured Properties</h1>
       <div>
-        {propertiesForRent?.map((property) => <Property property={property} key={property.id} />)}
-
+        {/* {propertiesForRent?.map((property) => <Property property={property} key={property.id} />)} */}
+        <Property property={property}/>
       </div>
     </div>
   )
