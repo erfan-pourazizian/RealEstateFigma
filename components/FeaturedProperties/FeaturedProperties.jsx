742ecc8bbfc562140391ceb6431f1c22d555b5d2
unknown <erfan8202@gmail.com>
2022-09-03 04:53:26 +0430

    UI fixed

--- components/FeaturedProperties/FeaturedProperties.jsx
+++ components/FeaturedProperties/FeaturedProperties.jsx
@@ -5,7 +5,7 @@ const FeaturedProperties = ({ propertiesForRent }) => {
   return (
     <div className='mt-36 mx-10'>
       <h1 className='text-spSize1 text-sp-gray2 text-center font-semibold'>Featured Properties</h1>
-      <div className='mt-14 flex flex-wrap'>
+      <div className='mt-14 flex flex-wrap ml-4'>
         {propertiesForRent?.map((property) => <Property property={property} key={property.id} />)}
       </div>
       <div className="flex justify-center mt-9">
