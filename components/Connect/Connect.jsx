93e7d2cc9968ff0cfa186bdb26bba411c731a191
unknown <erfan8202@gmail.com>
2022-09-04 17:12:33 +0430

    radius fixed

--- components/Connect/Connect.jsx
+++ components/Connect/Connect.jsx
@@ -7,7 +7,7 @@ const Connect = () => {
         <h1 className='text-spSize1 text-white font-bold w-[59%]'>Get Connected with top agents near you and find your home</h1>
         <p className='text-xl text-white mt-4'>Get in touch or create account. We are 24/7 available</p>
       </div>
-        <button className=" bg-white w-[18%] px-4 py-2 h-[60px] rounded-sm">Find Agent</button>
+        <button className=" bg-white w-[18%] px-4 py-2 h-[60px] rounded">Find Agent</button>
     </div>
   )
 }
