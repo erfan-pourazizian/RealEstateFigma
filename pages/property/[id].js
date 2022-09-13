8a48989c69feec11a4374349266657be5a27ea7e
unknown <erfan8202@gmail.com>
2022-09-14 01:44:37 +0430

    SwiperHandler moved

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,8 +1,7 @@
 import verify from "../../public/Images/verify.png";
 import { baseUrl, fetchApi } from "../../utils/fetchApi";
 import Image from "next/image";
-import React, { useRef, useState } from "react";
-import SwiperHandler from "../../utils/swiperHandler";
+import SwiperHandler from "../../components/SwiperHandler/SwiperHandler";
 
 const PropertyDetails = ({
   propertyDetails: {
@@ -35,6 +34,33 @@ const PropertyDetails = ({
           <p className="font-bold text-lg">
             AED {price} {rentFrequency && `/${rentFrequency}`}
           </p>
+        </div>
+        <div className="flex mt-2 ">
+          <div className="flex">
+            <div className="mr-3 flex self-end ">
+              <Image src="/Images/sqft.png" width={20} height={20} alt="sqft" />
+            </div>
+            <p className="text-contactSize text-sp-gray2 ">
+              {area.toString().length > 5
+                ? area.toString().substring(0, 5)
+                : area}
+              sqft
+            </p>
+          </div>
+
+          <div className="flex">
+            <div className="mr-3 ml-4 flex self-end ">
+              <Image src="/Images/beds.png" width={20} height={20} alt="beds" />
+            </div>
+            <p className="text-contactSize text-sp-gray2 ">{rooms} </p>
+          </div>
+
+          <div className="flex">
+            <div className="mr-3 ml-7 flex self-end ">
+              <Image src="/Images/bath.png" width={20} height={20} alt="bath" />
+            </div>
+            <p className="text-contactSize text-sp-gray2 ">{baths} </p>
+          </div>
         </div>
       </div>
     </div>
