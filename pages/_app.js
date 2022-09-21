0a53f2f3c8785a8e5c90bf4be3a712b035d72a8e
unknown <erfan8202@gmail.com>
2022-09-21 10:40:50 +0430

    loading added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,34 @@
 import "../styles/globals.css";
 import "../components/Layout";
 import Layout from "../components/Layout";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+
+function MyApp({Component, pageProps}) {
+    NProgress.configure({showSpinner: false});
+
+    Router.events.on('routeChangeStart', () => {
+        NProgress.start();
+    });
+
+    Router.events.on('routeChangeComplete', () => {
+        NProgress.done();
+    });
+
+    return (
+        <>
+            <Head>
+                <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+                      integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
+                      crossOrigin='anonymous' referrerPolicy='no-referrer'/>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
 export default MyApp;
