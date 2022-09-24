43658ebc2f6c94928104c3c2fe3fd7c450c31888
unknown <erfan8202@gmail.com>
2022-09-24 09:32:45 +0330

    link added

--- components/Connect/Connect.jsx
+++ components/Connect/Connect.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 const Connect = () => {
   return (
     <div className='flex bg-sp-orange2 mt-40 mb-40 mx-16 px-24 py-10 rounded-[10px] items-center' id="connect">
@@ -5,7 +7,9 @@ const Connect = () => {
         <h1 className='text-spSize1 text-white font-bold w-[59%]'>Get Connected with top agents near you and find your home</h1>
         <p className='text-spSize2 text-white mt-4'>Get in touch or create account. We are 24/7 available</p>
       </div>
+      <Link href="/search">
         <button className=" bg-white w-[18%] px-4 py-2 h-[60px] rounded">Find Agent</button>
+      </Link>
     </div>
   )
 }
