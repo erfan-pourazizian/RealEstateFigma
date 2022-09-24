cb4020a5bf8eb030940ada4d6ffc8ad9c19c71d6
unknown <erfan8202@gmail.com>
2022-09-24 09:01:48 +0330

    option connected

---
+++ utils/OurServicesOptions.js
@@ -0,0 +1,15 @@
+export const houseOptions = {
+  title: "RENT-UP SERVICE",
+  mainParagraph: "We take the hassle out of finding your next tenant",
+  paragraph: "Everything from marketing your property to moving in your tenant is all handled by our team. You set the requirements and we screen the tenants to find you the perfect match. No upfront fees.",
+  buttonLink: "/search",
+  buttonName: "Learn more",
+};
+
+export const officeOptions = {
+  title: "MANAGEMENT SERVICE",
+  mainParagraph: "Ready for a profitable property management  service you can rely on?",
+  paragraph: "Test drive our tech-driven, service focused property management service  that delivers real clarity on your assets, and prioritizes your net profitability. Flat fee service without any hidden fees. Open 7 days a week.",
+  buttonLink: "/search",
+  buttonName: "Learn more",
+};
