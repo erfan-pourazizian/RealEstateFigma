1ab80960fa3fdf85ec66f2ea083fee71ed5c2483
unknown <erfan8202@gmail.com>
2022-09-24 07:02:27 +0330

    link to index.js added

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,16 +5,18 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div className='container flex px-7 py-6 shadow-sp-navbar fixed top-0 z-20 bg-white'>
-            <div className='flex justify-start ml-28'>
-                <Image
-                    src="/Images/rent-logo.webp"
-                    width={100}
-                    height={15}
-                    objectFit='contain'
-                    alt="logo"
-                />
-            </div>
+        <div className='container flex px-7 py-6 shadow-sp-navbar fixed top-0 z-20 bg-white cursor-pointer'>
+            <Link href={"/"}>
+                <div className='flex justify-start ml-28'>
+                    <Image
+                        src="/Images/rent-logo.webp"
+                        width={100}
+                        height={15}
+                        objectFit='contain'
+                        alt="logo"
+                    />
+                </div>
+            </Link>
             <div className='flex ml-auto mr-10'>
                 {dropDownOptions.map((dropDown) => {
                     return (
@@ -22,7 +24,7 @@ const Navbar = () => {
                     )
                 })}
                 <Link href={'/search'} >
-                <a className='inline-flex justify-center  self-center text-spSize2 font-medium mr-8'>Search Rentals</a>
+                    <a className='inline-flex justify-center  self-center text-spSize2 font-medium mr-8'>Search Rentals</a>
                 </Link>
                 <div className="border flex border-sp-orange rounded px-7 cursor-pointer">
                     <a className='text-sp-orange self-center text-contactSize'>Contact Us</a>
