9f19024a7f666494d678d49079d8c74ec74f5e62
unknown <erfan8202@gmail.com>
2022-09-24 09:29:19 +0330

    link added

--- components/FeaturedProperties/FeaturedProperties.jsx
+++ components/FeaturedProperties/FeaturedProperties.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Property from './Property'
 
 const FeaturedProperties = ({ propertiesForRent }) => {
@@ -7,9 +8,11 @@ const FeaturedProperties = ({ propertiesForRent }) => {
       <div className='mt-14 flex flex-wrap ml-4'>
         {propertiesForRent?.map((property) => <Property property={property} key={property.id} />)}
       </div>
-      <div className="flex justify-center mt-9">
-        <button className=" bg-sp-orange px-4 py-2  rounded text-white ml-2">See More Property </button>
-      </div>
+      <Link href="/search">
+        <div className="flex justify-center mt-9">
+          <button className=" bg-sp-orange px-4 py-2  rounded text-white ml-2">See More Property </button>
+        </div>
+      </Link>
     </div>
   )
 }
