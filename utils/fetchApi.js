9c2469c5779a265f0f3e24a4d1bf46d192c58840
unknown <erfan8202@gmail.com>
2022-09-24 06:18:58 +0330

    key changed

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "73058e7124msha049941532cd7e7p196941jsn1193a375a4c0",
+      "X-RapidAPI-Key": "6a160a8f43msh75bebe39bb99d30p11c03cjsn5c7874bd8692",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
@@ -13,32 +13,3 @@ export const fetchApi = async (url) => {
   return data;
 };
 
-// import axios from "axios";
-
-// const options = {
-//   method: "GET",
-//   url: "https://bayut.p.rapidapi.com/properties/list",
-//   params: {
-//     locationExternalIDs: "5002,6020",
-//     purpose: "for-rent",
-//     hitsPerPage: "6",
-//     page: "0",
-//     lang: "en",
-//     sort: "city-level-score",
-//     rentFrequency: "monthly",
-//     categoryExternalID: "4",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": "73058e7124msha049941532cd7e7p196941jsn1193a375a4c0",
-//     "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-//   },
-// };
-// export async function getProperties() {
-//   try {
-//     const response = await axios.request(options);
-//     console.log(response.data.hits);
-//     return response.data;
-//   } catch (error) {
-//     return response.data;
-//   }
-// }
