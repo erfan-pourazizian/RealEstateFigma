c7d1104c635e7456836e9012c94c4b121750c9b8
unknown <erfan8202@gmail.com>
2022-09-24 08:19:50 +0330

    inspect bug resolved and refactor

--- components/OurServices/OurServices.jsx
+++ components/OurServices/OurServices.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const services = () => {
   return (
@@ -9,16 +10,15 @@ const services = () => {
       </div>
 
       <div className='flex mt-36 '>
-        <div className='grid grid-flow-col auto-cols-max gap-4  rounded'>
-          <div className="col-span-4  bg-sp-gray3 -z-10 w-[185%]">
-            <div className='ml-32 my-36 '>
-              <h1 className="text-rentH1Size text-sp-gray2 font-bold">RENT-UP SERVICE</h1>
+          <div className="bg-sp-gray3 absolute w-[70%] py-[150px] mt-3 rounded px-24">
+          <h1 className="text-rentH1Size text-sp-gray2 font-bold">RENT-UP SERVICE</h1>
               <p className='text-spSize2 text-sp-gray2 mt-5'>We take the hassle out of finding your next tenant</p>
               <p className='text-sp-gray2 text-sm mt-5'>Everything from marketing your property to moving in your tenant is all<br /> handled by our team. You set the requirements and we screen the tenants<br /> to find you the perfect match. No upfront fees.</p>
-              <button className="font-semibold bg-sp-orange px-6 mt-5 py-1 h-[45px] rounded-sm text-white mr-auto">Learn more</button>
-            </div>
+              <Link href={"/search"}>
+                <button className="font-semibold bg-sp-orange px-6 mt-5 py-1 h-[45px] rounded-sm text-white mr-auto cursor-pointer">Learn more</button>
+              </Link>
           </div>
-          <div className="col-start-5 col-end-9">
+          <div className="ml-auto mr-40">
             <div className='relative w-[600px] h-[560px] z-10 bottom-10 left-32'>
               <Image
                 alt='house'
@@ -27,32 +27,28 @@ const services = () => {
               />
             </div>
           </div>
-        </div>
       </div>
 
       <div className='flex mt-36 '>
-        <div className='grid grid-flow-col auto-cols-max gap-4 relative '>
-          <div className="col-start-1 col-end-4">
-            <div className='relative w-[600px] h-[560px] z-10 top-10 right-32 ml-32'>
+          <div className="mr-auto ml-40">
+            <div className='relative w-[600px] h-[560px] z-10 top-10 right-32'>
               <Image
                 alt='house'
-                src={"/Images/services-office.webp"}
+                src={"/Images/services-house.webp"}
                 layout="fill"
               />
             </div>
           </div>
-          <div className="col-start-2  bg-sp-gray3 -z-10 absolute w-[228%] rounded">
-            <div className='mr-32 my-36 text-right  '>
-              <h1 className="text-rentH1Size text-sp-gray2 mt-4 font-bold">MANAGEMENT SERVICE</h1>
-              <p className='text-spSize2 text-sp-gray2 mt-5'>Ready for a profitable property management <br/> service you can rely on?</p>
-              <p className='text-sp-gray2 text-sm mt-5'>Test drive our tech-driven, service focused property management service <br/> that delivers real clarity on your assets, and prioritizes your net profitability.<br/> Flat fee service without any hidden fees. Open 7 days a week.</p>
-              <button className="font-semibold bg-sp-orange px-6 mt-5 py-1 h-[45px] rounded-sm text-white mr-auto">Learn more</button>
-            </div>
-
+          <div className="bg-sp-gray3 absolute w-2/3 py-[150px] rounded px-24 text-right right-0 mr-20 mt-3">
+          <h1 className="text-rentH1Size text-sp-gray2 font-bold">RENT-UP SERVICE</h1>
+              <p className='text-spSize2 text-sp-gray2 mt-5'>We take the hassle out of finding your next tenant</p>
+              <p className='text-sp-gray2 text-sm mt-5'>Everything from marketing your property to moving in your tenant is all<br /> handled by our team. You set the requirements and we screen the tenants<br /> to find you the perfect match. No upfront fees.</p>
+              <Link href={"/search"}>
+                <button className="font-semibold bg-sp-orange px-6 mt-5 py-1 h-[45px] rounded-sm text-white mr-auto cursor-pointer">Learn more</button>
+              </Link>
           </div>
-        </div>
       </div>
-
+     
     </div>
   )
 }
