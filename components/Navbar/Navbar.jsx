89654ff232f96f525cfa04bd84cfd0a3942e7093
unknown <erfan8202@gmail.com>
2022-09-27 13:58:06 +0330

    link added to contact-me

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,19 +5,17 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div className='container flex px-7 py-6 shadow-sp-navbar fixed top-0 z-20 bg-white cursor-pointer'>
+        <div className='w-full flex py-6 shadow-sp-navbar fixed top-0 z-20 bg-white cursor-pointer px-[5%]'>
             <Link href={"/"}>
-                <div className='flex justify-start ml-28'>
+                <div className='flex self-center relative w-24 h-8'>
                     <Image
                         src="/Images/rent-logo.webp"
-                        width={100}
-                        height={15}
-                        objectFit='contain'
+                        layout='fill'
                         alt="logo"
                     />
                 </div>
             </Link>
-            <div className='flex ml-auto mr-10'>
+            <div className='flex ml-auto '>
                 {dropDownOptions.map((dropDown) => {
                     return (
                         <DropDown dropDownDetails={dropDown} key={dropDown.name} />
@@ -26,11 +24,11 @@ const Navbar = () => {
                 <Link href='/search' >
                     <a className='inline-flex justify-center  self-center text-spSize2 font-medium mr-8 hover:text-sp-orange3 transition duration-500 ease-in-out'>Search Rentals</a>
                 </Link>
-                <Link href='/contact-me'>
+                <a href='https://www.linkedin.com/in/erfan-pourazizian/' target={'_blank'} rel="noreferrer">
                     <button className="border flex border-sp-orange rounded px-7 py-2 cursor-pointer hover:bg-sp-orange3 transition duration-500 ease-in-out text-sp-orange self-center text-contactSize hover:text-white">
                         Contact Us
                     </button>
-                </Link>
+                </a>
             </div>
         </div>
     )
