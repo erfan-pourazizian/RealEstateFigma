448bf7cb517b879a02bf8057ca092099901ca671
unknown <erfan8202@gmail.com>
2022-09-27 13:53:45 +0330

    ul li added

--- components/Navbar/LinkHandler.jsx
+++ components/Navbar/LinkHandler.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 
 
-const LinkHandler = ({link, option}) => {
+const LinkHandler = ({ link, option }) => {
     return (
         <Link href={link}>
-            <a className="block px-4 py-2 text-gray-800 hover:bg-sp-orange3 transition duration-500 ease-in-out hover:text-white rounded">
-                {option}
-            </a>
+            <li>
+                <a className="block px-4 py-2 text-gray-800 hover:bg-sp-orange3 transition duration-300 ease-in-out hover:text-white rounded">
+                    {option}
+                </a>
+            </li>
         </Link>
     )
 }
