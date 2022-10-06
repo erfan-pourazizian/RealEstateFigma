759e864faee7ecf999b7387bb4afc37fa9dcad51
unknown <erfan8202@gmail.com>
2022-10-06 15:45:26 +0330

    animation added

--- components/Footer/LinkHandler.jsx
+++ components/Footer/LinkHandler.jsx
@@ -1,6 +1,6 @@
 const LinkHandler = ({link, name}) => {
     return (
-        <a href={link} className="my-1 text-sp-gray2 hover:text-sp-orange3 transition duration-500 ease-in-out text-sm">{name}</a>
+        <a href={link} className="my-1 w-fit text-sp-gray2 hover:text-sp-orange3 hover:translate-x-2 transition duration-500 ease-in-out text-sm">{name}</a>
     )
 }
 
