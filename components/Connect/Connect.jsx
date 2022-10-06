540f551a9aeb6dfb734618ac006cfca45ee2271d
unknown <erfan8202@gmail.com>
2022-10-06 22:00:18 +0330

    fix font for mobile view

--- components/Connect/Connect.jsx
+++ components/Connect/Connect.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const Connect = () => {
   return (
-    <div className='flex bg-sp-orange2 mt-24 sm:mt-40 mb-24 sm:mb-40 mx-[5%] px-[4%] sm:px-[6%] py-4 sm:py-10 rounded-[10px] items-center' id="connect">
+    <div className='flex bg-sp-orange2 mt-16 sm:mt-40 mb-16 sm:mb-40 mx-[5%] px-[4%] sm:px-[6%] py-4 sm:py-10 rounded-[10px] items-center' id="connect">
       <div className="sm:grow">
-        <h1 className='text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-spSize1 text-white font-medium sm:font-bold w-full sm:w-[75%] md:w-[59%]'>Get Connected with top agents near you and find your home</h1>
+        <h1 className='text-sm sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-spSize1 text-white font-medium sm:font-bold w-full sm:w-[75%] md:w-[59%]'>Get Connected with top agents near you and find your home</h1>
         <p className='text-xs sm:text-sm lg:text-base xl:text-lg 2xl:text-spSize2 text-white mt-2 sm:mt-4'>Get in touch or create account. We are 24/7 available</p>
       </div>
       <Link href="/search">
