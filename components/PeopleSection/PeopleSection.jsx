35ba4f22c68c32f2461a41875bc15bcdcfba1634
unknown <erfan8202@gmail.com>
2022-10-06 14:50:42 +0330

    responsive completed

--- components/PeopleSection/PeopleSection.jsx
+++ components/PeopleSection/PeopleSection.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const PeopleSection = () => {
   return (
-    <div className='relative mt-36 mx-10' id='peopleSection'>
+    <div className='relative mt-24 sm:mt-36 mx-[3%]' id='peopleSection'>
 
       <div className="absolute left-10 w-[160px] h-[130px]">
         <Image
@@ -12,20 +12,20 @@ const PeopleSection = () => {
         />
       </div>
       <h1 className="text-spSize1 text-sp-gray2 text-center font-semibold">What People Say About Us</h1>
-      <div className="grid grid-cols-2  place-items-center mt-14 mx-20 ">
-        <div className="relative w-full  h-[360px]">
+      <div className="flex flex-col sm:grid sm:grid-cols-2 mt-8 sm:mt-14 mx-[6%]">
+        <div className="relative w-full h-[270px] sm:h-[260px] md:h-[250px] lg:h-[360px]">
           <Image
             src="/Images/woman.webp"
             alt="woman"
             layout='fill'
           />
         </div>
-        <div className= "ml-16">
-          <h3 className='text-3xl font-bold text-sp-gray2'>Angelie Jolie</h3>
-          <div className="w-[80%]">
-            <p className='text-lg  text-sp-gray2 mt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div className="sm:ml-10 lg:ml-16 mt-5 sm:my-auto text-center sm:text-left align-middle my-auto flex flex-col">
+          <h3 className='text-3xl sm:text-2xl  lg:text-3xl font-bold text-sp-gray2'>Angelie Jolie</h3>
+          <div className="lg:w-[80%]">
+            <p className='text-base lg:text-lg  text-sp-gray2 mt-3 lg:mt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           </div>
-          <p className='text-xs text-sp-gray2 mt-7'>Home seller, Malang</p>
+          <p className='text-xs text-sp-gray2 mt-4 sm:mt-7'>Home seller, Malang</p>
         </div>
       </div>
     </div>
