2ae167fda24a3e74da8c1653a6b278cee037bcff
unknown <erfan8202@gmail.com>
2022-10-13 00:33:07 +0330

    key changed-2

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "6a160a8f43msh75bebe39bb99d30p11c03cjsn5c7874bd8692",
+      "X-RapidAPI-Key": "73058e7124msha049941532cd7e7p196941jsn1193a375a4c0",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
