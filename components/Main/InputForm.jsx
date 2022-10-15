94cf27da166998002d9216c6b858c9bb00a6e43f
unknown <erfan8202@gmail.com>
2022-10-15 05:53:05 +0330

    dotted-icon added with responsive

--- components/Main/InputForm.jsx
+++ components/Main/InputForm.jsx
@@ -1,11 +1,11 @@
 import InputHolder from './InputHolder'
 import { options } from '../../utils/options'
 import router from "next/router";
-
+import Image from 'next/image'
 
 const InputForm = () => {
     return (
-        <div className='flex flex-col py-1 absolute z-10 bg-white shadow-sp-input rounded mt-[15rem] md:mt-[19rem] xl:mt-[21rem] lg:mt-[16rem] mr-4'>
+        <div className='flex flex-col py-1 absolute  bg-white shadow-sp-input rounded mt-[15rem] md:mt-[19rem] xl:mt-[21rem] lg:mt-[16rem] mr-4'>
             <p className="ml-8 text-base xl:text-spSize2 text-[#4F4F4F] mt-5">For Current rent estimate, please provide the following
                 :</p>
             <div className="flex xl:flex-row flex-col mb-5">
@@ -14,7 +14,7 @@ const InputForm = () => {
                     {options.map(opt => {
                         return (
                             <div
-                                className="col-span-2 xl:col-span-2 [&:nth-child(1)]:col-span-4 xl:[&:nth-child(1)]:col-span-7 xl:[&:nth-child(2)]:w-[140px]  xl:[&:nth-child(6)]:ml-2 [&:nth-child(3)]:col-span-6 sm:[&:nth-child(3)]:col-span-2 xl:[&:nth-child(3)]:col-start-1 xl:[&:nth-child(3)]:col-end-4"
+                                className="col-span-2 xl:col-span-2 [&:nth-child(1)]:col-span-4 xl:[&:nth-child(1)]:col-span-7 xl:[&:nth-child(2)]:w-[140px] xl:[&:nth-child(6)]:ml-2 [&:nth-child(3)]:col-span-6 sm:[&:nth-child(3)]:col-span-2 xl:[&:nth-child(3)]:col-start-1 xl:[&:nth-child(3)]:col-end-4"
                                 key={opt.inputName}>
                                 <InputHolder inputName={opt.inputName} img={opt.img} alt={opt.alt} options={opt.opt}
                                     filter={opt} />
@@ -28,6 +28,12 @@ const InputForm = () => {
                 // onClick={}
                 >Submit
                 </button>
+            </div>
+            <div className='absolute -bottom-3 sm:-bottom-7 lg:-bottom-12 xl:-bottom-16 2xl:-bottom-20 -left-3 sm:-left-5 lg:-left-9 xl:-left-10 2xl:-left-14 w-14 sm:w-20 lg:w-32 xl:w-36 2xl:w-40 h-14 sm:h-20 lg:h-32 xl:h-36 2xl:h-40 md:-z-10'>
+                <Image src="/Images/dotted-square-1.png" alt="dotted-square" layout='fill'  />
+            </div>
+            <div className='absolute hidden md:flex md:-top-1 lg:-top-2 md:-right-1 lg:-right-3 md:w-12 lg:w-20 md:h-12 lg:h-20 z-10'>
+                <Image src="/Images/dotted-square-2.png" alt="dotted-square" layout='fill'  />
             </div>
         </div>
     )
